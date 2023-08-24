30de050dc16aea747e8c89214d42eaaf5ae27ae2
Flash Sheridan <fs@matterlabs.dev>
2023-08-24 21:50:33 +0000

    Do not merge:  Trim test

--- llvm/test/Iterate_llc.sh
+++ llvm/test/Iterate_llc.sh
@@ -6,9 +6,9 @@ date
 
 pushd ..
 
-cargo run --bin compiler-tester -- --mode='M0B0 0.8.20' --load-system-contracts system-contracts-stable-build -D    --zksolc '../compiler-solidity/target/debug/zksolc'  --zkvyper '../compiler-vyper/target/debug/zkvyper'
+cargo run --bin compiler-tester -- --mode='M0B0 0.8.20'  --path='tests/solidity/simple/default.sol' --load-system-contracts system-contracts-stable-build -D    --zksolc '../compiler-solidity/target/debug/zksolc'  --zkvyper '../compiler-vyper/target/debug/zkvyper'
 date
-for file in ./debug/*.unoptimized.ll; do
+for file in llvm/llvm/test/CodeGen/SyncVM/tests_solidity_complex_defi_Mooniswap_MooniswapPool_Mooniswap.sol.Mooniswap.optimized.ll; do
   valgrind   -q --trace-children=yes --error-exitcode=42 --suppressions=/home/zekebot/Documents/Code/compiler-tester/llvm/llvm/Valgrind_suppressions.supp   ./target-llvm/build-final/bin/llc -O3 "${file}" -o test.zasm
 done
 
@@ -16,3 +16,5 @@ popd
 
 echo Done at
 date
+echo _______________________
+echo
